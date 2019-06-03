e7c18c82a6435cdee16031f5b511788b3596086e
Miguel Chan <vvchan@outlook.com>
2019-06-03 12:16:15 +0800

    Modified cmake

--- src/includes/font_render.h
+++ src/includes/font_render.h
@@ -96,7 +96,7 @@ void FontRender::InitFont(const GLchar* path)
 		texture,
 		glm::ivec2(face->glyph->bitmap.width, face->glyph->bitmap.rows),
 		glm::ivec2(face->glyph->bitmap_left, face->glyph->bitmap_top),
-		face->glyph->advance.x
+		static_cast<GLuint>(face->glyph->advance.x)
 	};
 	Characters.insert(std::pair<GLchar, Character>(c, character));
 	}
